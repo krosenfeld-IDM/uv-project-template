06740c9baf89db3a536ea4641ed27f7ca1561842
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-05-29 10:41:24 -0700

    Update postCreateCommands.sh

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,3 +1,6 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+nvm install node
+
 pip install --upgrade pip > /tmp/pip.stdout.log 2> /tmp/pip.stderr.log
 pip install uv > /tmp/uv.stdout.log 2> /tmp/uv.stderr.log
 uv tool install tox --with tox-uv > /tmp/tox.stdout.log 2> /tmp/tox.stderr.log
